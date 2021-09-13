711b0a9753621d88d767a0d96847ef565ef1cc7a
Folfy_Blue <folfyblu@gmail.com>
2021-09-13 15:12:57 +0200

    Handling corrupted files

--- main.py
+++ main.py
@@ -1,17 +1,12 @@
 # For this script to work, you need
-# to edit line 49 of libs\anvilparser\anvil\empty_region.py
+# to edit line 49 of libs\anvilparser\anvil\empty_region.py ( First line of function inside() )
 # to return True because one of the checks is messed up
 # https://github.com/matcool/anvil-parser/issues/27
 
 import libs.anvilparser.anvil as anvil
-import os
-import re
 
-inputDir = "./input/"
-outputDir = "./output/"
-
-def removeEmptyChunks(regionX,regionZ):
-	region = anvil.Region.from_file(inputDir+'r.'+str(regionX)+"."+str(regionZ)+'.mca')
+def removeEmptyChunks(regionX,regionZ,directory):
+	region = anvil.Region.from_file(directory+'r.'+str(regionX)+"."+str(regionZ)+'.mca')
 	newRegion = anvil.EmptyRegion(regionX,regionZ)
 	isEmpty = True
 	for chunkX in range(0,32):
@@ -31,13 +26,23 @@ def removeEmptyChunks(regionX,regionZ):
 		return newRegion
 
 if __name__ == "__main__":
+	import os
+	import re
+
+	inputDir = "./input/"
+	outputDir = "./output/"
+
 	for item in os.scandir(inputDir):
 		if item.path.endswith(".mca") and item.is_file(): # if it's a mca file...
 			regionCoords = re.findall('r\.(-?\d+)\.(-?\d+)\.mca',item.name)[0] # Extract the region coordinates from the file name
 			if regionCoords:
-				region = removeEmptyChunks(regionCoords[0],regionCoords[1])
-				if region:
-					print(item.name+" has been cleaned! Saving..")
-					region.save(outputDir+item.name)
-				else:
-					print("Removing file '"+item.name+"' as it contains nothing but empty chunks.")
+				try:
+					region = removeEmptyChunks(regionCoords[0],regionCoords[1], inputDir)
+					if region:
+						print(item.name+" has been cleaned! Saving..")
+						region.save(outputDir+item.name)
+					else:
+						print("Removing file '"+item.name+"' as it contains nothing but empty chunks.")
+				except nbt.nbt.MalformatedFileError:
+					print("ERROR! File '"+item.name+"' contains corrupted or malformated data! We therefore cannot shrink it!")
+					os.system("cp "+inputDir+item.name+" "+outputDir+item.name)
